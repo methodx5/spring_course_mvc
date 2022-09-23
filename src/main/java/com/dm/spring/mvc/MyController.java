7e07a3eada1bb19191057c1d2b1939dc83341404
John Doe <methodvr3@gmail.com>
2022-09-23 23:03:40 +0300

    Study of spring (Spring_MVC) day 10

--- src/main/java/com/dm/spring/mvc/MyController.java
+++ src/main/java/com/dm/spring/mvc/MyController.java
@@ -3,11 +3,16 @@ package com.dm.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
 
     @RequestMapping("/")
@@ -16,21 +21,29 @@ public class MyController {
     }
 
     @RequestMapping("/askDetails")
-    public String askEmployeeDetails() {
+    public String askEmployeeDetails(Model model) {
+        model.addAttribute("employee", new Employee());
+
         return "ask-emp-details-view";
     }
 
+
 //    @RequestMapping("/showDetails")
-//    public String showEmpDetails() {
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        model.addAttribute("description", " - karate instructor");
 //        return "show-emp-details-view";
 //    }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model) {
-        String empName = request.getParameter("employeeName");
-        empName = "Mr. " + empName;
-        model.addAttribute("nameAttribute", empName);
-        model.addAttribute("description", " - karate instructor");
-        return "show-emp-details-view";
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        } else {
+            return "show-emp-details-view";
+        }
     }
 }
--- src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 
 <html>
@@ -9,7 +10,25 @@
 <br>
 <%--Your name:${param.employeeName}--%>
 
-Your name: ${nameAttribute}${description}
+Your name: ${employee.name}
+<br><br>
+Your surname: ${employee.surname}
+<br><br>
+Your salary: ${employee.salary}
+<br><br>
+Your department: ${employee.department}
+<br><br>
+Your car: ${employee.carBrand}
+<br><br>
+Language(s):
+<ul>
+    <c:forEach var ="lang" items="${employee.languages}">
+        <li>${lang}</li>
+    </c:forEach>
+</ul>
+<br><br>
+Phone Number: ${employee.phoneNumber}
+Email: ${employee.email}
 
 </body>
 </html>
